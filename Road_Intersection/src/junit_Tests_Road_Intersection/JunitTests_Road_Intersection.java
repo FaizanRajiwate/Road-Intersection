fd53965065662af385be7b13144bde88aa436fb9
mittulmongia <mittulmongia4@gmail.com>
2023-03-05 19:33:53 +0000

    Just Added the sample test to make sure Tests are working

---
+++ Road_Intersection/src/junit_Tests_Road_Intersection/JunitTests_Road_Intersection.java
@@ -0,0 +1,13 @@
+package junit_Tests_Road_Intersection;
+import org.junit.jupiter.api.Test;
+
+public class JunitTests_Road_Intersection {
+	
+	@Test
+	public void SampleTest()
+	{	
+		
+		
+	}
+
+}
