946245c0da857546f6608170621b3fa537c43be7
mittulmongia <mittulmongia4@gmail.com>
2023-03-06 23:57:25 +0000

    Update JunitTests_Road_Intersection.java
    Added 4 tests for checking CSV files not found that throws exception. Functionality to be added

--- Road_Intersection/src/junit_Tests_Road_Intersection/JunitTests_Road_Intersection.java
+++ Road_Intersection/src/junit_Tests_Road_Intersection/JunitTests_Road_Intersection.java
@@ -1,13 +1,43 @@
 package junit_Tests_Road_Intersection;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
 
 public class JunitTests_Road_Intersection {
 	
-	@Test
-	public void SampleTest()
+	@Test  
+	@DisplayName("Test for the null file or file not found for Vehicles table.")
+	public void NullFile_ShouldThrowFileNotFound_VehiclesTable()
 	{	
 		
+		Vehicles vehicle = new Vehicles();
+		//add code here to test Null file Exception for reading Vehicles.csv file
+	}
+	
+	@Test  
+	@DisplayName("Test for checking if vehicles.csv is updated.")
+	public void WriteFile_ShouldAddVehicleDetails_VehiclesTable()
+	{	
 		
+		Vehicles vehicle = new Vehicles();
+		//add code here to test for writing Vehicles.csv file
+	}
+	
+	@Test  
+	@DisplayName("Test for the null file or file not found for Phases table.")
+	public void NullFile_ShouldThrowFileNotFound_PhasesTable()
+	{	
+		
+		Vehicles vehicle = new Vehicles();
+		//add code here to test Null file Exception for reading Intersection.csv file
 	}
 
+	@Test  
+	@DisplayName("Test for the null file or file not found for Statistics table.")
+	public void NullFile_ShouldThrowFileNotFound_StatisticsTable()
+	{	
+		
+		Vehicles vehicle = new Vehicles();
+		//add code here to test Null file Exception for reading Statistics.csv file
+	}
 }
