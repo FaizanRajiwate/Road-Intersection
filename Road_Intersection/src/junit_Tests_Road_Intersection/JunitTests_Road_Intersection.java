f444412afad77a7065460ef037a820b7a64b6774
mittulmongia <mittulmongia4@gmail.com>
2023-03-07 22:02:38 +0000

    Added Tests for Vehicles Table, Phases Table and Statistics Table. Along with that added test to make sure Vehicles are added in the vehicles.csv file.
    Added Tests for Vehicles Table, Phases Table and Statistics Table. Along with that added test to make sure Vehicles are added in the vehicles.csv file.

--- Road_Intersection/src/junit_Tests_Road_Intersection/JunitTests_Road_Intersection.java
+++ Road_Intersection/src/junit_Tests_Road_Intersection/JunitTests_Road_Intersection.java
@@ -1,43 +1,216 @@
 package junit_Tests_Road_Intersection;
+import static org.junit.Assert.assertTrue;
+
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Rule;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.rules.ExpectedException;
 
 
 public class JunitTests_Road_Intersection {
 	
-	@Test  
-	@DisplayName("Test for the null file or file not found for Vehicles table.")
-	public void NullFile_ShouldThrowFileNotFound_VehiclesTable()
+	  @Rule
+	  public ExpectedException exception = ExpectedException.none(); // has to be public
+	
+	@Test
+	@DisplayName("Test for the null file Exception for Vehicles table.")
+	public void NullFile_ShouldThrowNullPointerException_VehiclesTable()
 	{	
+		String strVehicleFile = null;
+		Assertions.assertThrows(NullPointerException.class, ()-> Vehicle.VehiclesTableTest(strVehicleFile));
+	}
+	
+	@Test
+	@DisplayName("Test for not throwing null file Exception for Vehicles table.")
+	public void NullFile_ShouldNotThrowNullPointerException_VehiclesTable()
+	{	
+		String strVehicleFile = "src/Vehicles.csv";
 		
-		Vehicles vehicle = new Vehicles();
-		//add code here to test Null file Exception for reading Vehicles.csv file
+		Assertions.assertDoesNotThrow(()->Vehicle.VehiclesTableTest(strVehicleFile),"Exception Handled");
+	}
+	
+	
+	@Test
+	@DisplayName("Test for the File Not Found for Vehicles table. Stop processing and return")
+	public void FileUnavailable_ShouldReturnFalseAndStop_VehiclesTable()
+	{	
+		String strVehicleFile = "";
+		
+		assertTrue(!Vehicle.VehiclesTableTest(strVehicleFile));
+	}
+	
+	@Test
+	@DisplayName("Test for the File Found for Vehicles table. Continue processing and return")
+	public void FileAvailable_ShouldContinueProcessing_VehiclesTable()
+	{	
+		String strVehicleFile = "src/Vehicles.csv";
+		
+		assertTrue(Vehicle.VehiclesTableTest(strVehicleFile));
+	}
+	
+	@Test
+	@DisplayName("Test for not throwing File Not Found Exception for Vehicles table,Because it is handled in the method itself.")
+	public void FileAvailable_ShouldNotThrowFileNotFoundException_VehiclesTable()
+
+	{	
+		String strVehicleFile = "";
+		
+		Assertions.assertDoesNotThrow(()->Vehicle.VehiclesTableTest(strVehicleFile),"Exception Handled");
+	}
+
+	
+	///////////// Phases table tests
+	@Test
+	@DisplayName("Test for the null file Exception for Phases table.")
+	public void NullFile_ShouldThrowNullPointerException_PhasesTable()
+	{	
+		String strPhasesFile = null;
+		
+		Assertions.assertThrows(NullPointerException.class, ()->Vehicle.PhasesTableTest(strPhasesFile));
+	}
+	
+	@Test
+	@DisplayName("Test for not throwing null file Exception for Phases table.")
+	public void NullFile_ShouldNotThrowNullPointerException_PhasesTable()
+	{	
+		String strPhasesFile = "src/Intersection.csv";
+		
+		Assertions.assertDoesNotThrow(()->Vehicle.PhasesTableTest(strPhasesFile),"Exception Handled");
+	}
+
+	@Test
+	@DisplayName("Test for the File Not Found for Phases table. Stop processing and return")
+	public void FileUnavailable_ShouldReturnFalseAndStop_PhasesTable()
+	{	
+		String strPhasesFile = "";
+		
+		assertTrue(!Vehicle.PhasesTableTest(strPhasesFile));
+	}
+
+	@Test
+	@DisplayName("Test for the File Found for Phases table. Continue processing and return")
+	public void FileAvailable_ShouldContinueProcessing_PhasesTable()
+	{	
+		String strPhasesFile = "src/Intersection.csv";
+		
+		assertTrue(Vehicle.PhasesTableTest(strPhasesFile));
+	}
+	
+	
+	@Test
+	@DisplayName("Test for not throwing File Not Found Exception for Phases table,Because it is handled in the method itself.")
+	public void FileAvailable_ShouldNotThrowFileNotFoundException_PhasesTable()
+
+	{	
+		String strPhasesFile = "";
+		
+		Assertions.assertDoesNotThrow(()->Vehicle.PhasesTableTest(strPhasesFile),"Exception Handled");
+	}
+	///////////// Phases table tests Ends
+	
+///////////// Statistics table tests
+	@Test
+	@DisplayName("Test for the null value of Statistics table."	)
+	public void LoadData_StatisticsTable_NullValues()
+	{	
+		String[][] statisticsTableUnderTest = null;
+		String[][] statisticsTableActual = Vehicle.StatisticsTableTest();
+		Assertions.assertNotEquals(statisticsTableUnderTest, statisticsTableActual);
+		
+	}
+	
+	@Test
+	@DisplayName("Test for the data of Statistics table."	)
+	public void LoadData_StatisticsTable()
+	{	
+		String statisticsTableUnderTest[][] = {{"S1", "600s", "2000m", "20s"},
+		        {"S2", "60s", "300m", "10s"},
+		        {"S3", "300s", "1500m", "15s"},
+		        {"S4", "40s", "100m", "10s"},};
+		        
+				String  statisticsTableActual[][] = Vehicle.StatisticsTableTest();
+				Assertions.assertArrayEquals(statisticsTableUnderTest, statisticsTableActual);
+		
+	}
+	
+	
+///////////// Statistics table tests Ends
+	
+	////////Add Data in the vehicle File
+	@Test
+	@DisplayName("Test for the null file Exception for Vehicles table.")
+	public void NullFile_ShouldThrowNullPointerException_AddVehiclesTable()
+	{	
+		String strVehicleFile = null;
+		Assertions.assertThrows(NullPointerException.class, ()-> Vehicle.AddDataIntoVehicleTable(strVehicleFile));
+	}
+	
+	@Test
+	@DisplayName("Test for not throwing null file Exception for Vehicles table.")
+	public void NullFile_ShouldNotThrowNullPointerException_AddVehiclesTable()
+	{	
+		String strVehicleFile = "src/Vehicles.csv";
+		Vehicle.setGUIAddVehicles();
+		Vehicle.addActionListenersAndSetFrame();
+        		
+		Assertions.assertDoesNotThrow(()->Vehicle.AddDataIntoVehicleTable(strVehicleFile),"Exception Handled");
 	}
 	
 	@Test  
 	@DisplayName("Test for checking if vehicles.csv is updated.")
 	public void WriteFile_ShouldAddVehicleDetails_VehiclesTable()
-	{	
+	{	try
+		{
+		String strVehicleFile = "src/Vehicles.csv";
+		BufferedInputStream fis1 = new BufferedInputStream(new FileInputStream(strVehicleFile));
 		
-		Vehicles vehicle = new Vehicles();
-		//add code here to test for writing Vehicles.csv file
+		Vehicle.setGUIAddVehicles();
+		Vehicle.addActionListenersAndSetFrame();
+        
+		Vehicle.AddDataIntoVehicleTable(strVehicleFile);
+		BufferedInputStream fis2 = new BufferedInputStream(new FileInputStream(strVehicleFile));
+		long lngComparator = CompareBothFiles(fis1,fis2);
+		Assertions.assertEquals(-1, lngComparator);
+		}
+	catch(FileNotFoundException ex){
+		ex.printStackTrace();
+	     }
+		
 	}
 	
-	@Test  
-	@DisplayName("Test for the null file or file not found for Phases table.")
-	public void NullFile_ShouldThrowFileNotFound_PhasesTable()
-	{	
-		
-		Vehicles vehicle = new Vehicles();
-		//add code here to test Null file Exception for reading Intersection.csv file
+	private long CompareBothFiles(BufferedInputStream fis1, BufferedInputStream fis2)
+	{
+		try {
+			
+			int ch = 0;
+	        long pos = 1;
+	        while ((ch = fis1.read()) != -1) {
+	            if (ch != fis2.read()) {
+	                return pos;
+	            }
+	            pos++;
+	        }
+	        if (fis2.read() == -1) {
+	            return -1;
+	        }
+	        else 
+	        {
+	            return pos;
+	        }
+			
+		}
+		catch(IOException ex){
+			ex.printStackTrace();
+		     }
+		return 0;
+				
 	}
-
-	@Test  
-	@DisplayName("Test for the null file or file not found for Statistics table.")
-	public void NullFile_ShouldThrowFileNotFound_StatisticsTable()
-	{	
-		
-		Vehicles vehicle = new Vehicles();
-		//add code here to test Null file Exception for reading Statistics.csv file
-	}
+	
+	
 }
---
+++ Road_Intersection/src/junit_Tests_Road_Intersection/Vehicle.java
@@ -0,0 +1,487 @@
+package junit_Tests_Road_Intersection;
+
+import java.awt.*;
+import java.awt.event.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.*;
+import java.io.IOException;
+import javax.swing.*;
+import javax.swing.table.*;
+import org.junit.jupiter.api.Test;
+
+public class Vehicle {
+    // frame
+    static JFrame f;
+    static JFrame addVehiclesFrame;
+    static JFrame statisticsFrame;
+    // Table
+    static JTable vehiclesTable;
+    static JTable phasesTable;
+    static JTable statisticsTable;
+    static JTable addVehiclesTable;
+    //Table Model
+    static DefaultTableModel vehiclesModel;
+    static String strFileNameVehicles = "src/Vehicles.csv";
+    static String strFileNamePhases = "src/Intersection.csv";
+    //static String strFileNameVehicles = "";
+    
+    public static void main(String[] args) {
+    	Vehicle vehicle = new Vehicle();
+        // Frame initialization
+        f = new JFrame();
+        // Frame Title
+        f.setTitle("CourseWork");
+        f.setVisible(true);
+        //Labels
+        JLabel vehiclesLabel = new JLabel("Vehicles");
+        JLabel phasesLabel = new JLabel("Phases");
+        Font font = new Font("Courier", Font.BOLD, 20);
+        vehiclesLabel.setFont(font);
+        phasesLabel.setFont(font);
+        //Buttons
+        JButton Add = new JButton("Add Vehicles");
+        Add.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                
+                //addVehiclesTable();
+                setGUIAddVehicles();
+                addActionListenersAndSetFrame();
+                AddDataIntoVehicleTable(strFileNameVehicles);
+            }
+        });
+
+        JButton statistics = new JButton("View Statistics");
+        statistics.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                statisticsFrame = new JFrame();
+                statisticsFrame.setTitle("View Statistics");
+                statisticsFrame.setSize(650, 650);
+                statisticsFrame.setVisible(true);
+                //StatisticsTable();
+                StatisticsTableTest();
+                setGUIStatistics();
+            }
+        });
+
+        JButton Exit = new JButton("Exit");
+        Exit.addActionListener((event) -> System.exit(0));
+
+        //methods to get data from CSV files
+        //VehiclesTable(strFileNameVehicles);
+        
+        VehiclesTableTest(strFileNameVehicles);
+        PhasesTableTest(strFileNamePhases);
+        
+        //////////////////////
+       // PhasesTable();
+        //GUI Layout
+        f.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(-120, -250, 130, 300);
+
+        // column 0
+        c.gridx = 0;
+
+        // row 0
+        c.gridy = 0;
+
+        f.add(vehiclesLabel, c);
+        // column 1
+        c.gridx = 1;
+
+        f.add(phasesLabel, c);
+        // column 0
+        c.gridx = 0;
+
+        // row 2
+        c.gridy = 1;
+
+        // increases components width by 20 pixels
+        c.ipadx = 20;
+
+        // increases components height by 20 pixels
+        c.ipady = 10;
+        f.add(new JScrollPane(vehiclesTable), c);
+
+        // column 1
+        c.gridx = 1;
+        f.add(new JScrollPane(phasesTable), c);
+        // column 0
+        c.gridx = 0;
+
+        // row 3
+        c.gridy = 2;
+
+        f.add(Add, c);
+        // column 1
+        c.gridx = 1;
+
+        // row 3
+        c.gridy = 2;
+
+        f.add(statistics, c);
+        // column 1
+        c.gridx = 2;
+
+        // row 3
+        c.gridy = 2;
+
+        f.add(Exit, c);
+        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void VehiclesTable(String strFileName) {
+        String thisLine = "";
+        // Column Names
+        String[] columnNames = {"Vehicles", "Type", "Crossing Time", "Direction", "Length", "Emission", "Status",
+            "Segment"};
+        // Initializing the JTable
+        vehiclesModel = new DefaultTableModel();
+        vehiclesModel.setColumnIdentifiers(columnNames);
+        vehiclesTable = new JTable(vehiclesModel);
+        int i = 0;// line count of csv
+        String[][] data = new String[0][];// csv data line count=0 initially
+        try {
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader(strFileName));
+            while ((thisLine = br.readLine()) != null) // returns a Boolean value
+            {
+                i++;
+                String[][] newdata = new String[i][2];// create new array for data
+                String lineContent[] = thisLine.split(",");// get contents of line as an array
+                newdata[i - 1] = lineContent;// add new line to the array
+                System.arraycopy(data, 0, newdata, 0, i - 1);// copy previously read values to new array
+                data = newdata;// set new array as csv data
+                vehiclesModel.addRow(data[i - 1]);
+            }
+
+        } catch (IOException e) {
+           e.printStackTrace();
+           }
+    }
+
+    public static void PhasesTable() {
+        String thisLine = "";
+        int i = 0;// line count of csv
+        String[][] data = new String[0][];// csv data line count=0 initially
+        try {
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader("src/Intersection.csv"));
+            while ((thisLine = br.readLine()) != null) // returns a Boolean value
+            {
+                i++;
+                String[][] newdata = new String[i][2];// create new array for data
+                String lineContent[] = thisLine.split(",");// get contents of line as an array
+                newdata[i - 1] = lineContent;// add new line to the array
+                System.arraycopy(data, 0, newdata, 0, i - 1);// copy previously read values to new array
+                data = newdata;// set new array as csv data
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Column Names
+        String[] columnNames = {"Phases", "Duration"};
+        // Initializing the JTable
+        phasesTable = new JTable(data, columnNames);
+        phasesTable.getColumnModel().getColumn(0).setMaxWidth(80);
+        phasesTable.getColumnModel().getColumn(1).setMaxWidth(80);
+    }
+
+    public static void StatisticsTable() {
+        JTextField emissionsTF = new JTextField("10805 kg");
+        JLabel emissionsLabel = new JLabel("Emissions");
+        String data[][] = {{"S1", "600s", "2000m", "20s"},
+        {"S2", "60s", "300m", "10s"},
+        {"S3", "300s", "1500m", "15s"},
+        {"S4", "40s", "100m", "10s"},};
+        String[] columnNames = {"Segment", "Waiting Time", "Waiting Length", "Cross Time"};
+        statisticsTable = new JTable();
+        statisticsTable = new JTable(data, columnNames);
+        
+        JButton Exit = new JButton("Exit");
+
+        Exit.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                statisticsFrame.dispose();
+            }
+        }
+        );
+        statisticsFrame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        //column 0
+        c.gridx =1;
+
+        // row 1
+        c.gridy = 0;
+        c.ipadx = 15;
+        statisticsFrame.add(emissionsLabel,c);
+        //column 0
+        c.gridx = 0;
+
+        // row 0
+        c.gridy = 0;
+        c.ipadx = 0;
+        statisticsFrame.add(new JScrollPane(statisticsTable), c);
+        //column 2
+        c.gridx = 2;
+
+        // row 0
+        c.gridy = 0;
+        statisticsFrame.add(emissionsTF,c);
+        //column 0
+        c.gridx = 0;
+
+        // row 1
+        c.gridy = 1;
+        c.gridwidth = 0;
+        statisticsFrame.add(Exit, c);
+    }
+    
+    public static void setGUIAddVehicles()
+    {
+    	addVehiclesFrame = new JFrame();
+        addVehiclesFrame.setTitle("Add Vehicles");
+        addVehiclesFrame.setSize(650, 650);
+        addVehiclesFrame.setVisible(true);
+    	 String[] colHeadings = {"Vehicle", "Type", "Crossing Time", "Direction", "Length", "Emission", "Status", "Segment"};
+         int numRows = 1;
+         
+         DefaultTableModel model = new DefaultTableModel(numRows, colHeadings.length);
+         model.setColumnIdentifiers(colHeadings);
+         addVehiclesTable = new JTable(model);
+         
+    }
+    
+    
+
+    public static void addActionListenersAndSetFrame() {
+    	JButton Add = new JButton("Add Vehicles");
+        Add.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+            	AddDataIntoVehicleTable(strFileNameVehicles);
+            }
+        }
+        );
+        JButton Exit = new JButton("Exit");
+
+        Exit.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                addVehiclesFrame.dispose();
+            }
+        }
+        );
+        addVehiclesFrame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        //column 0
+        c.gridx = 0;
+
+        // row 0
+        c.gridy = 0;
+        // c.gridwidth = 2;
+        addVehiclesFrame.add(new JScrollPane(addVehiclesTable), c);
+        //column 0
+        c.gridx = 0;
+
+        // row 1
+        c.gridy = 1;
+        c.gridwidth = 0;
+        addVehiclesFrame.add(Add, c);
+        //column 0
+        c.gridx = 1;
+
+        // row 2
+        c.gridy = 1;
+        c.gridwidth = 0;
+        addVehiclesFrame.add(Exit, c);
+    }
+
+    
+    public static void AddDataIntoVehicleTable(String strFileName) {
+    	String[] tableValues = new String[8];
+    	//write to file
+        try {
+            FileWriter writer = new FileWriter(strFileName, true);
+            System.out.println(writer.toString());
+            for (int i = 0; i < addVehiclesTable.getRowCount(); i++) {
+                for (int j = 0; j < addVehiclesTable.getColumnCount(); j++) {
+                    if (addVehiclesTable.isEditing()) {
+                        addVehiclesTable.getCellEditor().stopCellEditing();
+                    }
+                    if (tableCheaker()) {
+                        //write
+                        if (j <= 6) {
+                            tableValues[j] = addVehiclesTable.getValueAt(i, j).toString() + ",";
+                        }
+                        if (j == 7) {
+                            tableValues[j] = addVehiclesTable.getValueAt(i, j).toString() + "\n";
+                        }
+                        writer.append(tableValues[j]);
+                    } else {
+                        JOptionPane.showMessageDialog(null, "Please fill the entire table");
+                        break;
+                    }
+                }
+            }
+            writer.close();
+        } 
+        catch(NullPointerException ex) {}
+        catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        //Add the values to vehicles table on the main frame
+        if (tableCheaker()) {
+            Object[] rowData = new Object[addVehiclesTable.getColumnCount()];
+            for (int i = 0;
+                    i < addVehiclesTable.getColumnCount();
+                    i++) {
+                rowData[i] = addVehiclesTable.getValueAt(0, i);
+            }
+            vehiclesModel.addRow(rowData);
+        }
+    }
+    
+    
+    public static boolean tableCheaker() {//Used to check if any cell in the table is empty/null
+        for (int j = 0; j < addVehiclesTable.getColumnCount(); j++) {
+            Object value = addVehiclesTable.getValueAt(0, j);
+            if (value == null) {
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    public static boolean VehiclesTableTest(String strFileName) {
+    	if(strFileName == "") {return false;}
+    		
+        String thisLine = "";
+        // Column Names
+        String[] columnNames = {"Vehicles", "Type", "Crossing Time", "Direction", "Length", "Emission", "Status",
+            "Segment"};
+        // Initializing the JTable
+        vehiclesModel = new DefaultTableModel();
+        vehiclesModel.setColumnIdentifiers(columnNames);
+        vehiclesTable = new JTable(vehiclesModel);
+        int i = 0;// line count of csv
+        String[][] data = new String[0][];// csv data line count=0 initially
+        try {
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader(strFileName));
+            while ((thisLine = br.readLine()) != null) // returns a Boolean value
+            {
+                i++;
+                String[][] newdata = new String[i][2];// create new array for data
+                String lineContent[] = thisLine.split(",");// get contents of line as an array
+                newdata[i - 1] = lineContent;// add new line to the array
+                System.arraycopy(data, 0, newdata, 0, i - 1);// copy previously read values to new array
+                data = newdata;// set new array as csv data
+                vehiclesModel.addRow(data[i - 1]);
+            }
+
+        } 
+        catch (FileNotFoundException e) {
+           e.printStackTrace();
+
+        }
+        catch (IOException e) {
+          e.printStackTrace();
+       }
+		return true;
+    }
+
+    public static boolean PhasesTableTest(String strFileName) {
+    	if(strFileName == "") {return false;}
+        String thisLine = "";
+        int i = 0;// line count of csv
+        String[][] data = new String[0][];// csv data line count=0 initially
+        try {
+            //parsing a CSV file into BufferedReader class constructor  
+            BufferedReader br = new BufferedReader(new FileReader(strFileName));
+            while ((thisLine = br.readLine()) != null) // returns a Boolean value
+            {
+                i++;
+                String[][] newdata = new String[i][2];// create new array for data
+                String lineContent[] = thisLine.split(",");// get contents of line as an array
+                newdata[i - 1] = lineContent;// add new line to the array
+                System.arraycopy(data, 0, newdata, 0, i - 1);// copy previously read values to new array
+                data = newdata;// set new array as csv data
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        // Column Names
+        String[] columnNames = {"Phases", "Duration"};
+        // Initializing the JTable
+        phasesTable = new JTable(data, columnNames);
+        phasesTable.getColumnModel().getColumn(0).setMaxWidth(80);
+        phasesTable.getColumnModel().getColumn(1).setMaxWidth(80);
+        return true;
+    }
+    
+    private static void setGUIStatistics()
+    {
+    	JTextField emissionsTF = new JTextField("10805 kg");
+        JLabel emissionsLabel = new JLabel("Emissions");
+    	JButton Exit = new JButton("Exit");
+
+        Exit.addActionListener(new ActionListener() {
+
+            public void actionPerformed(ActionEvent e) {
+                statisticsFrame.dispose();
+            }
+        }
+        );
+        
+        statisticsFrame.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        //column 0
+        c.gridx =1;
+
+        // row 1
+        c.gridy = 0;
+        c.ipadx = 15;
+        statisticsFrame.add(emissionsLabel,c);
+        //column 0
+        c.gridx = 0;
+
+        // row 0
+        c.gridy = 0;
+        c.ipadx = 0;
+        statisticsFrame.add(new JScrollPane(statisticsTable), c);
+        //column 2
+        c.gridx = 2;
+
+        // row 0
+        c.gridy = 0;
+        statisticsFrame.add(emissionsTF,c);
+        //column 0
+        c.gridx = 0;
+
+        // row 1
+        c.gridy = 1;
+        c.gridwidth = 0;
+        statisticsFrame.add(Exit, c);
+    }
+    
+    public static String[][] StatisticsTableTest() {
+        
+        String data[][] = {{"S1", "600s", "2000m", "20s"},
+        {"S2", "60s", "300m", "10s"},
+        {"S3", "300s", "1500m", "15s"},
+        {"S4", "40s", "100m", "10s"},};
+        String[] columnNames = {"Segment", "Waiting Time", "Waiting Length", "Cross Time"};
+        statisticsTable = new JTable();
+        statisticsTable = new JTable(data, columnNames);
+        
+        return data;
+    }
+
+    
+    
+}
